57f1da64718e48a79e2f95bb1b339d2ee8824acb
Nirmal Patel <nirmal@Nirmals-MacBook-Air.local>
2023-06-09 22:28:38 +0530

    change in home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
  from-black via-black to-gray-800'>
 
         <div className=' text-white max-w-screen-lg mx-auto flex flex-col
-        items-center justify-center h-full px-4 md:flex-row
+        items-center justify-between h-full px-4 md:flex-row
         '>
             <div className='flex flex-col justify-center h-full'>
                 <h2 className='text-4xl sm:text-7xl font-bold text-white'>
