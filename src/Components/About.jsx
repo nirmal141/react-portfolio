7cd1d5aaed4d0e1ea60009469063255bcf39256d
Nirmal Patel <nirmal@Nirmals-MacBook-Air.local>
2023-06-09 22:23:48 +0530

    initial commit

---
+++ src/Components/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name='about' className='h-screen w-full text-white bg-gradient-to-b from-gray-800 to-black'>
+
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+
+            <p className='text-xl mt-20'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam amet omnis eligendi excepturi! Quia accusantium nulla dolorem? Soluta id recusandae veniam adipisci cupiditate, earum sapiente neque tenetur sunt quaerat quis.</p>
+            <br/>
+
+            <p className='text-xl '>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque expedita dolor consequatur quam officia reprehenderit similique temporibus quidem voluptates sunt esse placeat, cum minima pariatur molestias vel beatae iusto cumque. Magnam eveniet voluptatibus ut. Quos reiciendis minus ipsa error! Eos iusto quae, molestiae modi perferendis ad dolor aspernatur quo numquam.
+            </p>
+
+        </div>
+
+
+
+    </div>
+  )
+}
+
+export default About
